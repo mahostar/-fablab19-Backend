55fe0b430d34aab94cd5c1d9505f6976513a107f
mahostar <mouhamedwassouma@gmail.com>
2025-11-23 04:16:01 +0100

    Revert SMTP config to use env vars (Port 587)

--- emailService.js
+++ emailService.js
@@ -4,15 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || 'smtp.gmail.com',
-    port: 465, // Use port 465 for SSL
-    secure: true, // Use SSL
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     },
-    // Force IPv4 to avoid Gmail IPv6 issues on some hosting platforms
-    family: 4
 });
 
 export const sendEmail = async (to, subject, html) => {
